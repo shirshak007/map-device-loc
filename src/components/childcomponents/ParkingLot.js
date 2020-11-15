88a77f251b154ddd3ddefa8eadab5745e26c20a7
Shirshak Das <shirshak.das007@gmail.com>
2020-11-15 19:36:54 +0530

    grid organize complete

--- src/components/childcomponents/ParkingLot.js
+++ src/components/childcomponents/ParkingLot.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles, Grid, useTheme } from "@material-ui/core";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
 import Axios from "axios";
 import qs from "qs";
 import LoadingAnimation from "./LoadingAnimation";
@@ -7,19 +9,43 @@ import LoadingAnimation from "./LoadingAnimation";
 const useStyles = makeStyles((theme) => ({
   container: {
     display: "flex",
-    flexDirection: "column",
+    flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    padding: theme.spacing(2),
   },
   parkingcontainer: {
     display: "flex",
     flexDirection: "row",
     width: "1000px",
     height: "700px",
-    padding: theme.spacing(2),
-    backgroundColor: theme.palette.warning.light,
+    left: "0px",
+    top: "10px",
+    background: "#b5e7a0",
     position: "relative",
+
+    [theme.breakpoints.down("sm")]: {
+      width: "640px",
+      height: "480px",
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "420px",
+      height: "300px",
+    },
+  },
+  baytextrotate: {
+    fontSize: "1em",
+
+    transform: "rotate(-90deg)",
+    webkitTransform: "rotate(-89deg)",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "8px",
+    },
+  },
+  baytext: {
+    fontSize: "1em",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "8px",
+    },
   },
 }));
 
@@ -28,6 +54,9 @@ export default function ParkingLot(props) {
   const theme = useTheme();
   const [Found, setFound] = useState(false);
   const [Loading, setLoading] = useState(true);
+  const matchesMD = useMediaQuery("(min-width:970px)");
+  const matchesSM = useMediaQuery("(min-width:610px)");
+  const matchesXS = useMediaQuery("(min-width:500px)");
   //getting data from previous view
   const DeviceID = props.match.params.DeviceID;
   //BayData
@@ -64,29 +93,128 @@ export default function ParkingLot(props) {
       {Loading ? <LoadingAnimation /> : ""}
       {Found ? (
         <Grid container className={classes.parkingcontainer}>
-          {BayData.map((bay) => (
-            <div
-              key={bay.id}
-              style={{
-                position: "absolute",
-                left: bay.x,
-                top: bay.y,
-                backgroundColor: bay.active
-                  ? theme.palette.error.main
-                  : theme.palette.success.dark,
-                height: bay.height,
-                width: bay.width,
-                cursor: "pointer",
-                textAlign: "center",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                color: "white",
-              }}
-            >
-              {bay.active ? "Occupied" : "Free"}
-            </div>
-          ))}
+          {BayData.map((bay) => {
+            if (
+              `${matchesMD}` === "true" &&
+              `${matchesSM}` === "true" &&
+              `${matchesXS}` === "true"
+            ) {
+              return (
+                <div
+                  key={bay.id}
+                  style={{
+                    position: "absolute",
+                    left: bay.x,
+                    top: bay.y,
+                    backgroundColor: bay.active
+                      ? theme.palette.error.main
+                      : theme.palette.success.dark,
+                    height: bay.height,
+                    width: bay.width,
+                    cursor: "pointer",
+                    textAlign: "center",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    color: "white",
+                  }}
+                >
+                  <div
+                    className={
+                      bay.width > bay.height
+                        ? classes.baytextrotate
+                        : classes.baytext
+                    }
+                  >
+                    {bay.active ? "Occupied" : "Free"}
+                    <br />
+                    H:{bay.height}
+                    <br /> W:{bay.width}
+                    <br />
+                    X:{bay.x} <br /> Y:{bay.y}
+                  </div>
+                </div>
+              );
+            } else if (
+              `${matchesMD}` === "false" &&
+              `${matchesSM}` === "true" &&
+              `${matchesXS}` === "true"
+            ) {
+              return (
+                <div
+                  key={bay.id}
+                  style={{
+                    position: "absolute",
+                    left: bay.x / 1.5,
+                    top: bay.y / 1.5,
+                    backgroundColor: bay.active
+                      ? theme.palette.error.main
+                      : theme.palette.success.dark,
+                    height: bay.height / 1.5,
+                    width: bay.width / 1.5,
+                    cursor: "pointer",
+                    textAlign: "center",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    color: "white",
+                  }}
+                >
+                  <div
+                    className={
+                      bay.width > bay.height
+                        ? classes.baytextrotate
+                        : classes.baytext
+                    }
+                  >
+                    {bay.active ? "Occupied" : "Free"}
+                    <br />
+                    H:{bay.height}
+                    <br /> W:{bay.width}
+                    <br />
+                    X:{bay.x} <br /> Y:{bay.y}
+                  </div>
+                </div>
+              );
+            } else {
+              return (
+                <div
+                  key={bay.id}
+                  style={{
+                    position: "absolute",
+                    left: bay.x / 2.5,
+                    top: bay.y / 2.5,
+                    backgroundColor: bay.active
+                      ? theme.palette.error.main
+                      : theme.palette.success.dark,
+                    height: bay.height / 2,
+                    width: bay.width / 2,
+                    cursor: "pointer",
+                    textAlign: "center",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    color: "white",
+                  }}
+                >
+                  <div
+                    className={
+                      bay.width > bay.height
+                        ? classes.baytextrotate
+                        : classes.baytext
+                    }
+                  >
+                    {bay.active ? "Occupied" : "Free"}
+                    <br />
+                    H:{bay.height}
+                    <br /> W:{bay.width}
+                    <br />
+                    X:{bay.x} <br /> Y:{bay.y}
+                  </div>
+                </div>
+              );
+            }
+          })}
         </Grid>
       ) : (
         ""
